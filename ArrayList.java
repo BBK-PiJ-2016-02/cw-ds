cc2cc784a2836d283b76f504b07636774c033cbe
Alun Davey <hey@4lun.io>
2017-01-08 21:29:05 +0000

    Complete ArrayList and tests

--- ArrayList.java
+++ ArrayList.java
@@ -1,26 +1,74 @@
 public class ArrayList implements List {
 
+	private final static int MAX_SIZE = 10000;
+	private Object[] data = new Object[MAX_SIZE];
+	private int count = 0;
+
 	public boolean isEmpty() {
 		return false;
 	}
 
 	public int size() {
-		return 0;
+		return count;
 	}
 
 	public ReturnObject get(int index) {
-		return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		if(size() == 0) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		}
+
+		if(isOutOfBounds(index)) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}
+
+		return new ReturnObjectImpl(data[index]);
 	}
 
 	public ReturnObject remove(int index) {
-		return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		if(isOutOfBounds(index)) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}
+
+		ReturnObjectImpl item = new ReturnObjectImpl(data[index]);
+		data[index] = null;
+
+		for(int i = index; i < size() - 1; i++) {
+			data[i] = data[i+1];
+		}
+
+		data[size() - 1] = null;
+		count--;
+
+		return item;
 	}
 
 	public ReturnObject add(int index, Object item) {
-		return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		if(isOutOfBounds(index) || MAX_SIZE == size()) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}
+
+		for(int i = size() - 1; i >= index; i--) {
+			data[i+1] = data[i];
+		}
+
+		data[index] = item;
+		count++;
+
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
 	}
 
 	public ReturnObject add(Object item) {
-		return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		if(MAX_SIZE == size()) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}
+
+		data[count] = item;
+		count++;
+
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+	}
+
+	private boolean isOutOfBounds(int index) {
+		return (index >= size() || index < 0);
 	}
 }
--- ArrayListTest.java
+++ ArrayListTest.java
@@ -1,10 +1,36 @@
 public class ArrayListTest {
 
 	public static void main(String[] args) {
-		ArrayList arrayList1 = new ArrayList();
+        ArrayList arrayList1 = new ArrayList();
         System.out.println("arrayList1.size() should return 0: " + arrayList1.size());
         System.out.println("arrayList1.get(1).hasError() should return true: " + arrayList1.get(1).hasError());
         System.out.println("arrayList1.get(1).getError() should return ErrorMessage.EMPTY_STRUCTURE: " + arrayList1.get(1).getError());
+
+        ArrayList arrayList2 = new ArrayList();
+        arrayList2.add("Jack");
+        arrayList2.add("Jill");
+        System.out.println("arrayList2.size() should return 2: " + arrayList2.size());
+        System.out.println("arrayList2.get(1).hasError() should return false: " + arrayList2.get(1).hasError());
+        System.out.println("arrayList2.get(1).getReturnValue() should return \"Jill\": " + arrayList2.get(1).getReturnValue());
+        arrayList2.remove(0);
+        System.out.println("arrayList2.size() should return 1: " + arrayList2.size());
+        System.out.println("arrayList2.get(1).hasError() should return true: " + arrayList2.get(1).hasError());
+        System.out.println("arrayList2.get(0).getReturnValue() should return \"Jill\": " + arrayList2.get(0).getReturnValue());
+        arrayList2.remove(0);
+        System.out.println("arrayList2.size() should return 0: " + arrayList2.size());
+
+        ArrayList arrayList3 = new ArrayList();
+        arrayList3.add("Adam");
+        arrayList3.add("Eve");
+        arrayList3.add("Roger");
+        arrayList3.add("Julie");
+        System.out.println("arrayList3.size() should return 4: " + arrayList3.size());
+        System.out.println("arrayList3.get(2).getReturnValue() should return \"Roger\": " + arrayList3.get(2).getReturnValue());
+        arrayList3.add(2, "Alice");
+        System.out.println("arrayList3.get(2).getReturnValue() should return \"Alice\": " + arrayList3.get(2).getReturnValue());
+        System.out.println("arrayList3.get(3).getReturnValue() should return \"Roger\": " + arrayList3.get(3).getReturnValue());
+        System.out.println("arrayList3.get(4).getReturnValue() should return \"Julie\": " + arrayList3.get(4).getReturnValue());
+        System.out.println("arrayList3.size() should return 5: " + arrayList3.size());
 	}
 
 }
